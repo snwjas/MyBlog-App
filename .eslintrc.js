ac70aa3c928e95fc182ddb69ddbc7ddcde3f31a7
Myles.Yang <myles.yang@foxmail.com>
2022-12-07 22:00:31 +0800

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   },
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
 
+  ignorePatterns: ["src/plugin/*"],
+
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
